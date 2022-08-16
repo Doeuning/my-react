a243c6d0a875014c435060e6d233f7e2dcab208c
doeunkim <doeuning@gmail.com>
2022-08-16 16:54:13 +0900

    수정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 function Movie(props) {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
   const apiKey = "c79521c72b6690785c671f45832e339b";
   const userId = "doeuning";
   const sessionId = "5cb7458b6a9c2bc68202d97a55537df2f004fce6";
-  const [page, setPage] = useState(1);
 
   function isBottom(el) {
     return (
@@ -14,12 +14,12 @@ function Movie(props) {
   }
   function trackingBottom() {
     if (isBottom(document.getElementById("App"))) {
-      setPage((num) => (num = num + 1));
-      getData();
-      document.removeEventListener("scroll", trackingBottom);
+      setPage((prev) => prev + 1);
+      // window.removeEventListener("scroll", trackingBottom);
     }
   }
   function getData() {
+    console.log("get data");
     fetch(
       `https://api.themoviedb.org/3//account/${userId}/rated/tv?api_key=${apiKey}&session_id=${sessionId}&page=${page}`
     )
@@ -28,7 +28,16 @@ function Movie(props) {
       })
       .then(function (datas) {
         setData(data.concat(datas.results));
-        console.log(datas);
+        console.log(
+          "현재 페이지 -----",
+          datas.page,
+          "전체 페이지 -----",
+          datas.total_pages
+        );
+        if (datas.page === datas.total_pages) {
+          console.log("remove scroll event");
+          window.removeEventListener("scroll", trackingBottom);
+        }
       })
       .catch((error) => {
         console.log(error.message);
@@ -37,8 +46,13 @@ function Movie(props) {
   }
   useEffect(() => {
     window.addEventListener("scroll", trackingBottom);
-    getData();
+    return () => {
+      window.removeEventListener("scroll", trackingBottom);
+    };
   }, []);
+  useEffect(() => {
+    getData();
+  }, [page]);
 
   return (
     <>
@@ -48,9 +62,10 @@ function Movie(props) {
             <li key={el.id} className={props.name}>
               <img
                 src={`https://image.tmdb.org/t/p/original${el.poster_path}`}
-                alt={el.original_title}
+                alt={el.original_name}
               />
-              <h4>{el.original_title}</h4>
+              <h4>{el.original_name}</h4>
+              <h4>{el.rating}</h4>
             </li>
           ))
         ) : (
